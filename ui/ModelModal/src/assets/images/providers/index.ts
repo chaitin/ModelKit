6b477dbd6373c6b3fbf457e0795d55fe0caddd2e
jiangwel <jiangwei9910@gmail.com>
2025-09-16 18:20:06 +0800

    chore: readme

---
+++ ui/ModelModal/src/assets/images/providers/index.ts
@@ -0,0 +1,2 @@
+export { default as NomicLogo } from './nomic.png'
+export { default as YoudaoLogo } from './netease-youdao.svg'
--- ui/ModelModal/src/constants/models.ts
+++ ui/ModelModal/src/constants/models.ts
@@ -1,96 +1,95 @@
-// Vision models
-export const visionAllowedModels = [
-  'llava',
-  'moondream',
-  'minicpm',
+export const visionModels = [
+  'chatgpt-4o(?:-[\\w-]+)?',
+  'claude-3',
+  'claude-opus-4',
+  'claude-sonnet-4',
+  'deepseek-vl(?:[\\w-]+)?',
+  'doubao-seed-1[.-]6(?:-[\\w-]+)?',
   'gemini-1\\.5',
   'gemini-2\\.0',
   'gemini-2\\.5',
   'gemini-exp',
-  'claude-3',
-  'claude-sonnet-4',
-  'claude-opus-4',
-  'vision',
+  'gemma-3(?:-[\\w-]+)',
+  `gemma3(?:[-:\\w]+)?`,
   'glm-4(?:\\.\\d+)?v(?:-[\\w-]+)?',
-  'qwen-vl',
-  'qwen2-vl',
-  'qwen2.5-vl',
-  'qwen2.5-omni',
-  'qvq',
-  'internvl2',
-  'grok-vision-beta',
-  'grok-4(?:-[\\w-]+)?',
-  'pixtral',
   'gpt-4(?:-[\\w-]+)',
   'gpt-4.1(?:-[\\w-]+)?',
-  'gpt-4o(?:-[\\w-]+)?',
   'gpt-4.5(?:-[\\w-]+)',
+  'gpt-4o(?:-[\\w-]+)?',
   'gpt-5(?:-[\\w-]+)?',
-  'chatgpt-4o(?:-[\\w-]+)?',
+  'grok-4(?:-[\\w-]+)?',
+  'grok-vision-beta',
+  'internvl2',
+  'kimi-latest',
+  'kimi-thinking-preview',
+  'kimi-vl-a3b-thinking(?:-[\\w-]+)?',
+  'llama-4(?:-[\\w-]+)?',
+  'llama-guard-4(?:-[\\w-]+)?',
+  'llava',
+  'minicpm',
+  'moondream',
   'o1(?:-[\\w-]+)?',
   'o3(?:-[\\w-]+)?',
   'o4(?:-[\\w-]+)?',
-  'deepseek-vl(?:[\\w-]+)?',
-  'kimi-latest',
-  'gemma-3(?:-[\\w-]+)',
-  'doubao-seed-1[.-]6(?:-[\\w-]+)?',
-  'kimi-thinking-preview',
-  `gemma3(?:[-:\\w]+)?`,
-  'kimi-vl-a3b-thinking(?:-[\\w-]+)?',
-  'llama-guard-4(?:-[\\w-]+)?',
-  'llama-4(?:-[\\w-]+)?',
+  'pixtral',
+  'qvq',
+  'qwen-vl',
+  'qwen2-vl',
+  'qwen2.5-omni',
+  'qwen2.5-vl',
   'step-1o(?:.*vision)?',
-  'step-1v(?:-[\\w-]+)?'
+  'step-1v(?:-[\\w-]+)?',
+  'vision'
 ]
 
-export const visionExcludedModels = [
+export const notVisionModels = [
+  'AIDC-AI/Marco-o1',
   'gpt-4-\\d+-preview',
   'gpt-4-turbo-preview',
   'gpt-4-32k',
   'gpt-4-\\d+',
   'o1-mini',
   'o3-mini',
-  'o1-preview',
-  'AIDC-AI/Marco-o1'
+  'o1-preview'
 ]
 
-export const FUNCTION_CALLING_EXCLUDED_MODELS = [
+export const NOT_TOOL_CALL_MODELS = [
   'aqa(?:-[\\w-]+)?',
-  'imagen(?:-[\\w-]+)?',
-  'o1-mini',
-  'o1-preview',
   'AIDC-AI/Marco-o1',
   'gemini-1(?:\\.[\\w-]+)?',
   'qwen-mt(?:-[\\w-]+)?',
+  'imagen(?:-[\\w-]+)?',
+  'o1-mini',
+  'o1-preview',
   'gpt-5-chat(?:-[\\w-]+)?'
 ]
 
-export const FUNCTION_CALLING_MODELS = [
+export const TOOL_CALL_MODELS = [
+  'claude',
+  'deepseek',
+  'doubao-seed-1[.-]6(?:-[\\w-]+)?',
+  'hunyuan',
   'gpt-4o',
   'gpt-4o-mini',
   'gpt-4',
   'gpt-4.5',
   'gpt-oss(?:-[\\w-]+)',
   'gpt-5(?:-[0-9-]+)?',
-  'o(1|3|4)(?:-[\\w-]+)?',
-  'claude',
-  'qwen',
-  'qwen3',
-  'hunyuan',
-  'deepseek',
   'glm-4(?:-[\\w-]+)?',
   'glm-4.5(?:-[\\w-]+)?',
-  'learnlm(?:-[\\w-]+)?',
-  'gemini(?:-[\\w-]+)?', // 提前排除了gemini的嵌入模型
+  'gemini(?:-[\\w-]+)?',
   'grok-3(?:-[\\w-]+)?',
-  'doubao-seed-1[.-]6(?:-[\\w-]+)?',
+  'o(1|3|4)(?:-[\\w-]+)?',
+  'qwen',
+  'qwen3',
+  'learnlm(?:-[\\w-]+)?',
   'kimi-k2(?:-[\\w-]+)?'
 ]
 
-export const PERPLEXITY_SEARCH_MODELS = [
-  'sonar-pro',
+export const PERPLEXITY_MODELS = [
   'sonar',
+  'sonar-deep-research',
+  'sonar-pro',
   'sonar-reasoning',
-  'sonar-reasoning-pro',
-  'sonar-deep-research'
+  'sonar-reasoning-pro'
 ]
